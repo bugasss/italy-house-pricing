6f6c601130cee2652b49ffead5cfc8c695a9bdeb
tommaso.ramella <tommaso.ramella90@gmail.com>
2023-05-29 00:19:44 +0200

    all

--- streamlit_app/streamlit_main.py
+++ streamlit_app/streamlit_main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from datetime import date
 import streamlit as st
+import plotly.express as px
 
 from maps_italy import MapPriceItaly
 from map_neighbourhoods import MapPriceNeighbourhoods
